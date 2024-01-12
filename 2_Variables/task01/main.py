7c9294e51a16155faf624499f637c2fc442b5a79
Musatov Nikita Malikovich <NMusatov@gksm.local>
2024-01-12 20:04:09 +0300

    ass solution Codeby 2_Variables - task 01

---
+++ 2_Variables/task01/main.py
@@ -0,0 +1,10 @@
+botnet = 1000
+everyday_minus = 2
+evereday_plus = 3
+days = 30
+
+answer_one = (botnet - (everyday_minus*days) + (evereday_plus*days))
+answer_two = (botnet - (everyday_minus*days))
+
+print('Первый ответ задания:', answer_one)
+print('Второй ответ задания:', answer_two)
