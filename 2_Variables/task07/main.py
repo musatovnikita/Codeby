1424d9181e3f1055e6841efcf005af1299055d28
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-16 13:10:52 +0300

    add solution Codeby 2_Variables - task 07

---
+++ 2_Variables/task07/main.py
@@ -0,0 +1,15 @@
+my_var1 = 5
+my_var2 = 3
+
+print(my_var1 + 4, my_var1 - 2, my_var1 * 5 - 3, my_var1 * 3 + 1, my_var1 * 3)
+print(my_var1 - 3, my_var1 * 4 + 1, my_var1 * 4, my_var1 * 3 - 1, my_var1 + 3)
+print(my_var1 * my_var1, my_var1 * 4 - 1, my_var1 * 2 + 3, my_var1 + 2,
+      my_var1 - 4)
+print(my_var1 * 3 + 3, my_var1 * 2 + 2, my_var1 + 1, my_var1,
+      my_var1 * my_var1 - 1)
+print(my_var1 * 2 + 1, my_var1 * 2, my_var1 - 1, my_var1 * my_var1 - 2,
+      my_var1 * 4 - 3)
+print()
+print(my_var2 - 1, my_var2 + 4, my_var2 + 3)
+print(my_var2 * 3, my_var2 + 2, my_var2 - 2)
+print(my_var2 + 1, my_var2 * 1, my_var2 + 5)
