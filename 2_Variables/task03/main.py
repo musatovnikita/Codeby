cd5cd0944093332477423a29635814cdff1ebe67
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-17 12:56:53 +0300

    add solution Codeby 2_Variables - task 03

--- 2_Variables/task03/main.py
+++ 2_Variables/task03/main.py
@@ -1,12 +1,13 @@
-rectangular_house = 7*9
-round_house = 3.14*9**2/4
-wrong_house = 8*9 - (2*3)
+area_yura = 7*9
+area_alex = 3.14*((9/2)** 2)
+area_vladimir = 6*3 + 6*8
 
-print('В самом маленьком доме живёт Александра: ', rectangular_house >
-      round_house)
-print('В самом маленьком доме живёт Юрий: ', rectangular_house < round_house)
-print('В самом маленьком доме живёт Владимир: ', round_house > wrong_house)
-print('В самом маленьком доме живёт Александра: ', round_house < wrong_house)
-print('В самом маленьком доме живёт Владимир: ', rectangular_house >
-      wrong_house)
-print('В самом маленьком доме живёт Юрий: ', rectangular_house < wrong_house)
+area_yura_min = area_yura < area_alex < area_vladimir
+area_alex_min = area_alex < area_yura < area_vladimir
+area_vladimir_min = area_vladimir < area_yura < area_yura
+
+print("Iura's area", area_yura)
+print("alex's area", area_alex)
+print("vladimir's area", area_vladimir)
+
+print("\nMinimal area is Iura's area", area_yura_min)
