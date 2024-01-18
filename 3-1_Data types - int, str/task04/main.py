73690cd086d53f140d4c404295d00daa72e8e015
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-18 14:09:17 +0300

    add solution Codeby 3-1_Data types - int, str - task 04

---
+++ 3-1_Data types - int, str/task04/main.py
@@ -0,0 +1,5 @@
+number_one, number_two = 721217, 123321
+
+print("Результат деления =", round(number_one/number_two, 2))
+print("Результат целочисленного деления =", number_one//number_two)
+print("Остаток от деления =", number_one%number_two)
