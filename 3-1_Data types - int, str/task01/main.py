4227478a8af39e16a0e6d251879ca0f4c52cf419
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-18 13:28:47 +0300

    add solution Codeby 3-1_Data types - int, str - task 01

---
+++ 3-1_Data types - int, str/task01/main.py
@@ -0,0 +1,4 @@
+language = "Языки программирования: Pyp, Python, Goland, Jaba..."
+
+print(language.replace("Pyp", "PHP").replace("Python", "PYTHON").replace(
+    "Goland", "GOLANG").replace("Jaba...", "JAVA."))
