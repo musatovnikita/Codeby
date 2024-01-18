db2e2d0645d41edd331ac247d0fffc159144e91f
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-18 15:43:24 +0300

    add solution Codeby 3-1_Data types - int, str - task 07

---
+++ 3-1_Data types - int, str/task07/main.py
@@ -0,0 +1,6 @@
+history = "История Python, одного из самых простых языков программирования, началась в 1989 году."
+
+print(history[32:35:1] + history[1:5:1])
+print(history[47:50:1] + history[1:5:1])
+print(history[32:35:1] + history[26:27:1] + history[2:5:1])
+print(history[32:33:1] + history[48:50:1] + history[35:37:1] + history[3:5:1])
