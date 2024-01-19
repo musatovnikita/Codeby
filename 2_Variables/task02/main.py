80421cec535bd4e04bf273c46d73d64fb42cfc8d
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-19 12:32:47 +0300

    add solution Codeby 3-1_Data types - int, str - task 02

--- 2_Variables/task02/main.py
+++ 2_Variables/task02/main.py
@@ -2,6 +2,6 @@ base = 4
 height = 3
 additionally = 1.5
 
-answer = (base**2 + height**2) / 5 + (additionally*2)
+answer = (base**2 + height**2) / 5 + additionally*2
 
 print('Георгию понадобится', answer, 'метров верёвки')
