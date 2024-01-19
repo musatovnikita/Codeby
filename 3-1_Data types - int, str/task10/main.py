8bc244be6475bdc5c792fabe723ba797fa665b41
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-19 12:47:46 +0300

    add solution Codeby 3-1_Data types - int, str - task 10

--- 3-1_Data types - int, str/task10/main.py
+++ 3-1_Data types - int, str/task10/main.py
@@ -1,5 +1,5 @@
 str_1, str_2, str_3 = "5qwerty", "3python", "9calendar"
 
-print(str_1, str_1.replace("5", 5*"q"))
-print(str_2, str_2.replace("3", 3*"p"))
-print(str_3, str_3.replace("9", 3*"c"))
+print(str_1, int(str_1[0:1:])*str_1[1:2:] + str_1[1::])
+print(str_2, int(str_2[0:1:])*str_2[1:2:] + str_2[1::])
+print(str_3, int(str_3[0:1:])*str_3[1:2:] + str_3[1::])
