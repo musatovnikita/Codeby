a352d9e36c52baf0bea68c9ed6326b2e784fb4a2
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-22 11:18:39 +0300

    add solution Codeby 3-1_Data types - int, str - task 06

--- 3-1_Data types - int, str/task06/main.py
+++ 3-1_Data types - int, str/task06/main.py
@@ -1,5 +1,3 @@
 sent = "---у_РОССИИ_естЬ_тОЛЬко_дВа_союзнника_—_ЕЁ_АРМИЯ_и_фЛОт==="
 
-print(sent.lstrip(sent[0:3:]).rstrip(sent[55::]).title().replace("_",
-                                                                 " " "").replace(
-    "Союзнника", "Союзника"))
+print(sent.strip("-=").title().replace("_", " ").replace("Союзнника", "Союзника"))
