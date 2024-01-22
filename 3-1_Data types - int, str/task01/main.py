f6f60a001c98f64fc17b32ef7d3fc90147fbae0d
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-22 17:08:38 +0300

    add solution Codeby 3-1_Data types - int, str - task 01

--- 3-1_Data types - int, str/task01/main.py
+++ 3-1_Data types - int, str/task01/main.py
@@ -1,6 +1,5 @@
 language = "Языки программирования: Pyp, Python, Goland, Jaba..."
 
-print(language[0:24:] + language[24:25:] + language[32:33:].upper() +
-      language[26:27:].upper() + language[27:29:] + language[29:37:].upper()
-      + language[37:42:].upper() + language[37:38:] + language[43:45:] +
-      language[45:53:].replace("Jaba...", "Java.").upper())
+print(language[:24] + language[24:-2].upper().replace("PYP",
+                                                         "PHP").replace(
+    "GOLAND", "GOLANG").replace("JABA", "JAVA"))
