f838044b2ff40ea21626f0cf6250b7e9bf6d0c6e
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-22 12:45:42 +0300

    add solution Codeby 2_Variables - task 02

--- 2_Variables/task02/main.py
+++ 2_Variables/task02/main.py
@@ -2,6 +2,6 @@ base = 4
 height = 3
 additionally = 1.5
 
-answer = (base**2 + height**2)**2**1/125 + additionally*2
+answer = (base**2 + height**2)**(1/2) + additionally*2
 
 print('Георгию понадобится', answer, 'метров верёвки')
