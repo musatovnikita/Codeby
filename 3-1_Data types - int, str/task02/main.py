d3d2b299795a9d2c452aedbcb19477eb3ba0ea0d
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-22 11:19:26 +0300

    add solution Codeby 3-1_Data types - int, str - task 02

--- 3-1_Data types - int, str/task02/main.py
+++ 3-1_Data types - int, str/task02/main.py
@@ -1,5 +1,6 @@
 expr = 1000/3 + 100/3 + 10/3
+expr_2 = 1000/3 + 100//3 + 10//3
 
 print(int(expr))
-print(round(expr))
-print(float(str(expr).replace("99999999999994", "3333333")))
+print(float(round(expr)))
+print(round(expr_2, 7))
