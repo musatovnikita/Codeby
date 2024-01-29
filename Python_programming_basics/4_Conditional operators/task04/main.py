b6df4b3430d507dce82031b58b7f7f84dca6477d
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-01-29 17:14:32 +0300

    add solution Codeby 4_Conditional operators - task 02, task 04 and task 05

--- Python_programming_basics/4_Conditional operators/task04/main.py
+++ Python_programming_basics/4_Conditional operators/task04/main.py
@@ -3,7 +3,7 @@ if (string := input("Введите строку: ")):
         print("Количество символов не должно быть больше 100!")
     elif len(string) % 10 == 1 and len(string) != 11:
         print("В строке", len(string), "символ")
-    elif len(string) % 10 in [2, 3, 4] and len(string) not in [12, 13, 14]:
+    elif len(string) % 10 in (2, 3, 4) and len(string) not in (12, 13, 14):
         print("В строке", len(string), "символа")
     else:
         print("В строке", len(string), "символов")
--- Python_programming_basics/4_Conditional operators/task05/main.py
+++ Python_programming_basics/4_Conditional operators/task05/main.py
@@ -1,15 +1,9 @@
-if 0 <= (hours := int(input("Введите часы: "))) <= 23:
-    if 0 <= (minutes := int(input("Введите минуты: "))) <= 59:
-        if 0 <= (seconds := int(input("Введите секунды: "))) <= 59:
+if 0 <= (hours := int(input("Введите часы: "))) <= 23 and\
+    0 <= (minutes := int(input("Введите минуты: "))) <= 59 and\
+    0 <= (seconds := int(input("Введите секунды: "))) <= 59:
             hours = "0" + str(hours)
             minutes = "0" + str(minutes)
             seconds = "0" + str(seconds)
             print(hours[-2:], minutes[-2:], seconds[-2:], sep=":")
-        else:
-            print(
-                "Введите числа для часов от 0 до 23, и для минут и секунд от 0 до 59")
-    else:
-        print(
-            "Введите числа для часов от 0 до 23, и для минут и секунд от 0 до 59")
 else:
     print("Введите числа для часов от 0 до 23, и для минут и секунд от 0 до 59")
