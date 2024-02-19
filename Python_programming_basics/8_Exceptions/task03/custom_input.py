5a5962b124d2bd3c1ec6861c88ef9f36217f094d
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-02-19 11:49:20 +0300

    add solution Codeby 8_Exceptions task 03, task 04

--- Python_programming_basics/8_Exceptions/task03/custom_input.py
+++ Python_programming_basics/8_Exceptions/task03/custom_input.py
@@ -1,8 +1,8 @@
 def get_input(prompt="Введите значение: ", data_type=int):
     try:
         user_input = input(prompt)
-        return data_type(user_input)
     except ValueError:
         print("Ошибка ввода!", f"Ввод пользователя должен быть {data_type}", sep="\n")
     else:
         print("Ввод успешен.")
+        return data_type(user_input)
--- Python_programming_basics/8_Exceptions/task04/main.py
+++ Python_programming_basics/8_Exceptions/task04/main.py
@@ -2,32 +2,28 @@ from custom_input import get_input
 import random
 
 def main():
-    try:
-        user_input = get_input("Введите число от 1 до 10: ", int)
-        if user_input < 1 or user_input > 10:
-            print("Введите число от 1 до 10.")
-            return
+    user_input = None
+    user_input = get_input("Введите число от 1 до 10: ", int)
+    if not 1 <= user_input <= 10:
+        print("Введите число от 1 до 10.")
+        return
 
-        random_number = random.randint(10, 100)
-        print("Случайное число:", random_number)
+    random_number = random.randint(10, 100)
+    print("Случайное число:", random_number)
 
-        start_number = min(user_input, random_number)
-        end_number = max(user_input, random_number)
+    start_number = min(user_input, random_number)
+    end_number = max(user_input, random_number)
 
-        total_sum = sum(range(start_number, end_number + 1))
+    total_sum = sum(range(start_number, end_number + 1))
 
-        print(f"Сумма чисел от {start_number} до {end_number}: {total_sum}")
+    print(f"Сумма чисел от {start_number} до {end_number}: {total_sum}")
 
-        if user_input == random_number:
-            print("Введенное число совпадает со случайным числом.")
-            return total_sum  # Вернуть сумму чисел в случае совпадения.
-
-    except KeyboardInterrupt:
-        print("\nПрограмма прервана пользователем.")
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
-
-    return None  # Если совпадений нет или возникла ошибка, вернуть None.
+    if user_input == random_number:
+        print("Введенное число совпадает со случайным числом.")
+        return total_sum  # Вернуть сумму чисел в случае совпадения.
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"Произошла критическая ошибка: {e}")
