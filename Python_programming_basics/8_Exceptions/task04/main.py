b056aacbd6ca239d97a29b5fa513f5aa54f53363
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-02-19 12:32:49 +0300

    add solution Codeby 8_Exceptions task 04

--- Python_programming_basics/8_Exceptions/task04/main.py
+++ Python_programming_basics/8_Exceptions/task04/main.py
@@ -1,29 +1,14 @@
-from custom_input import get_input
-import random
+from random import randint
 
-def main():
-    user_input = None
-    user_input = get_input("Введите число от 1 до 10: ", int)
-    if not 1 <= user_input <= 10:
-        print("Введите число от 1 до 10.")
-        return
 
-    random_number = random.randint(10, 100)
-    print("Случайное число:", random_number)
-
-    start_number = min(user_input, random_number)
-    end_number = max(user_input, random_number)
-
-    total_sum = sum(range(start_number, end_number + 1))
-
-    print(f"Сумма чисел от {start_number} до {end_number}: {total_sum}")
-
-    if user_input == random_number:
-        print("Введенное число совпадает со случайным числом.")
-        return total_sum  # Вернуть сумму чисел в случае совпадения.
-
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        print(f"Произошла критическая ошибка: {e}")
+try:
+    number = int(input("Введите число от 1 до 10: "))
+except ValueError as v:
+    print("Вы ввели строку -", v)
+else:
+    if (random_number := randint(10, 100)) == number:
+        print(number * 2)
+    elif number in range(1, 11):
+        print(sum(val for val in range(number, random_number + 1)))
+    else:
+        print('Введите число от 1 до 10')
