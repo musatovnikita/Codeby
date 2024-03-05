a64f899201686ed584bc883b2ce007a7f71f48ad
Musatov Nikita Malikovich <nikitamusatov@gmail.com>
2024-03-05 12:14:35 +0500

    add solution Codeby 11-1_Regular expressions task 01, task 04

--- Python_programming_basics/11-1_Regular expressions/task01/main.py
+++ Python_programming_basics/11-1_Regular expressions/task01/main.py
@@ -2,6 +2,6 @@ import re
 
 text = 'Московское время 10:36:06'
 
-matches = re.findall(r'\b(\d{2}:?\d{2}:?\d{2})\b', text)
+matches = re.findall(r'\b(\d{2}:?){3}\b', text)
 for match in matches:
     print(match)
